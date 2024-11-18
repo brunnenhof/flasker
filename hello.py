7027d1e5ef9a69a0b6ad3b235ebc24a1e1d73cef
Ulrich Goluke <git@blue-way.net>
2024-11-18 19:03:46 +0100

    initial commit

---
+++ hello.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+
+#def index():
+#	return "<h2>Hello World</h2>"
+
+def index():
+	first_name = "PeterPan"
+	stuff = "This is <strong>bold</strong>"
+	fav_pizza = ["Salami", "Knoblauch", "Tuna", 423]
+	return render_template("index.html", first_name=first_name,
+		stuff=stuff,
+		fav_pizza=fav_pizza)
+
+@app.route('/user/<name>')
+
+def user(name):
+	return render_template("user.html", user_name=name)
+
+
